ca734daa80202221e1becffcf1d12b9fd9f0fe36
Ryan Weaver <ryan@symfonycasts.com>
2023-11-07 09:16:39 -0500

    make:factory

---
+++ src/Factory/PlanetFactory.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Planet;
+use App\Repository\PlanetRepository;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+use Zenstruck\Foundry\RepositoryProxy;
+
+/**
+ * @extends ModelFactory<Planet>
+ *
+ * @method        Planet|Proxy                     create(array|callable $attributes = [])
+ * @method static Planet|Proxy                     createOne(array $attributes = [])
+ * @method static Planet|Proxy                     find(object|array|mixed $criteria)
+ * @method static Planet|Proxy                     findOrCreate(array $attributes)
+ * @method static Planet|Proxy                     first(string $sortedField = 'id')
+ * @method static Planet|Proxy                     last(string $sortedField = 'id')
+ * @method static Planet|Proxy                     random(array $attributes = [])
+ * @method static Planet|Proxy                     randomOrCreate(array $attributes = [])
+ * @method static PlanetRepository|RepositoryProxy repository()
+ * @method static Planet[]|Proxy[]                 all()
+ * @method static Planet[]|Proxy[]                 createMany(int $number, array|callable $attributes = [])
+ * @method static Planet[]|Proxy[]                 createSequence(iterable|callable $sequence)
+ * @method static Planet[]|Proxy[]                 findBy(array $attributes)
+ * @method static Planet[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
+ * @method static Planet[]|Proxy[]                 randomSet(int $number, array $attributes = [])
+ */
+final class PlanetFactory extends ModelFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function getDefaults(): array
+    {
+        return [
+            'description' => self::faker()->text(),
+            'lightYearsFromEarth' => self::faker()->randomFloat(),
+            'name' => self::faker()->text(255),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): self
+    {
+        return $this
+            // ->afterInstantiate(function(Planet $planet): void {})
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return Planet::class;
+    }
+}
---
+++ src/Factory/VoyageFactory.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Voyage;
+use App\Repository\VoyageRepository;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+use Zenstruck\Foundry\RepositoryProxy;
+
+/**
+ * @extends ModelFactory<Voyage>
+ *
+ * @method        Voyage|Proxy                     create(array|callable $attributes = [])
+ * @method static Voyage|Proxy                     createOne(array $attributes = [])
+ * @method static Voyage|Proxy                     find(object|array|mixed $criteria)
+ * @method static Voyage|Proxy                     findOrCreate(array $attributes)
+ * @method static Voyage|Proxy                     first(string $sortedField = 'id')
+ * @method static Voyage|Proxy                     last(string $sortedField = 'id')
+ * @method static Voyage|Proxy                     random(array $attributes = [])
+ * @method static Voyage|Proxy                     randomOrCreate(array $attributes = [])
+ * @method static VoyageRepository|RepositoryProxy repository()
+ * @method static Voyage[]|Proxy[]                 all()
+ * @method static Voyage[]|Proxy[]                 createMany(int $number, array|callable $attributes = [])
+ * @method static Voyage[]|Proxy[]                 createSequence(iterable|callable $sequence)
+ * @method static Voyage[]|Proxy[]                 findBy(array $attributes)
+ * @method static Voyage[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
+ * @method static Voyage[]|Proxy[]                 randomSet(int $number, array $attributes = [])
+ */
+final class VoyageFactory extends ModelFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function getDefaults(): array
+    {
+        return [
+            'leaveAt' => \DateTimeImmutable::createFromMutable(self::faker()->dateTime()),
+            'planet' => PlanetFactory::new(),
+            'purpose' => self::faker()->text(255),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): self
+    {
+        return $this
+            // ->afterInstantiate(function(Voyage $voyage): void {})
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return Voyage::class;
+    }
+}

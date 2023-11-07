112fa2554d1616d3f98fc5ec6e689ea821d09e50
Ryan Weaver <ryan@symfonycasts.com>
2023-11-07 09:15:44 -0500

    make:crud Voyage

---
+++ src/Controller/VoyageController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Voyage;
+use App\Form\VoyageType;
+use App\Repository\VoyageRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/voyage')]
+class VoyageController extends AbstractController
+{
+    #[Route('/', name: 'app_voyage_index', methods: ['GET'])]
+    public function index(VoyageRepository $voyageRepository): Response
+    {
+        return $this->render('voyage/index.html.twig', [
+            'voyages' => $voyageRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_voyage_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $voyage = new Voyage();
+        $form = $this->createForm(VoyageType::class, $voyage);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($voyage);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_voyage_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('voyage/new.html.twig', [
+            'voyage' => $voyage,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_voyage_show', methods: ['GET'])]
+    public function show(Voyage $voyage): Response
+    {
+        return $this->render('voyage/show.html.twig', [
+            'voyage' => $voyage,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_voyage_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Voyage $voyage, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(VoyageType::class, $voyage);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_voyage_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('voyage/edit.html.twig', [
+            'voyage' => $voyage,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_voyage_delete', methods: ['POST'])]
+    public function delete(Request $request, Voyage $voyage, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$voyage->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($voyage);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_voyage_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/VoyageType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Voyage;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VoyageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('purpose')
+            ->add('leaveAt')
+            ->add('planet')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Voyage::class,
+        ]);
+    }
+}

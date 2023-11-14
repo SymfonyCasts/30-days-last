b6bbcfca0e1e56e09649de2f1ca40e6a7f4871d7
Ryan Weaver <ryan@symfonycasts.com>
2023-11-14 11:37:06 -0500

    composer require babdev/pagerfanta-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
 ];

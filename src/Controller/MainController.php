e4942e1aa3f969e938775480fd86f0911ee23e23
Ryan Weaver <ryan@symfonycasts.com>
2023-11-17 16:33:12 -0500

    Using the cool MapQueryParameter

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,16 +7,19 @@ use App\Repository\VoyageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function homepage(VoyageRepository $voyageRepository, PlanetRepository $planetRepository, Request $request): Response
+    public function homepage(
+        VoyageRepository $voyageRepository,
+        PlanetRepository $planetRepository,
+        #[MapQueryParameter('query')] string $query = null,
+        #[MapQueryParameter('planets', \FILTER_VALIDATE_INT)] array $searchPlanets = [],
+    ): Response
     {
-        $query = $request->query->get('query');
-        $searchPlanets = array ($request->query->all()['planets'] ?? []);
-        $searchPlanets = array_filter($searchPlanets, 'is_numeric');
         $voyages = $voyageRepository->findBySearch($query, $searchPlanets);
 
         return $this->render('main/homepage.html.twig', [
